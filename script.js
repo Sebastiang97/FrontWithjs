3838cf2d73d29891c4529b722d6efe61196b7f59
Sebastiang97 <ssebastiang97@gmail.com>
2022-06-19 00:18:29 -0500

    Modify script to investigatemodify script to late

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //Sebas
+// https://developer.mozilla.org/es/docs/Web/API/Canvas_API/Manipulating_video_using_canvas
+
+// https://es.stackoverflow.com/questions/386330/como-obtener-una-imagen-de-video-en-html-js
+
+// https://developer.mozilla.org/es/docs/Web/API/URLSearchParams
