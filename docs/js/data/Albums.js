619769d951c51e8aebf82a04cf99101d6caa5abc
Sebastiang97 <ssebastiang97@gmail.com>
2022-06-20 21:53:08 -0500

    Add Albums and Bootstrap script Bundle

---
+++ docs/js/data/Albums.js
@@ -0,0 +1,44 @@
+const albums = [
+  {
+    id: 1,
+    album: 'All My life',
+    canciones: [
+      {
+        id: 1,
+        name: 'All My Life',
+        src: './assets/test/Foo_Fighters_All_My_Life.mp3',
+        urlImage: './assets/test/logo.jpg',
+        artist: 'Foo Fighters',
+        album: 'All My Life',
+        year: '1997',
+      },
+      {
+        id: 2,
+        name: 'My Hero',
+        src: './assets/test/Foo_Fighters_My_Hero.mp3',
+        urlImage: './assets/test/logo1.png',
+        artist: 'Foo Fighters',
+        album: 'The Colour and the Shape',
+        year: '1997',
+      },
+      {
+        id: 3,
+        name: 'Best of You',
+        src: './assets/test/Foo_Fighters_Best_Of_You.mp3',
+        urlImage: './assets/test/logo2.jpg',
+        artist: 'Foo Fighters',
+        album: 'The Colour and the Shape',
+        year: '1997',
+      },
+      {
+        id: 4,
+        name: 'Everlong',
+        src: './assets/test/Foo_Fighters_Everlong.mp3',
+        urlImage: './assets/test/logo3.jpg',
+        artist: 'Foo Fighters',
+        album: 'The Colour and the Shape',
+        year: '1997',
+      },
+    ],
+  },
+]
